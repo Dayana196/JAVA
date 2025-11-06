46c11e9e55a6d094f33e9eaf08efe836d3b080a5
Dayana Barbosa <dayanita1204bb@gmail.com>
2025-11-06 11:20:52 -0500

    :art: JAVA

---
+++ ejemplo/src/main/java/com/dayana/Main.java
@@ -0,0 +1,23 @@
+package com.dayana;
+
+public class Main {
+    public static void main(String[] args) {
+        int numero = 100;
+        long otroNumero = 10L;
+        boolean bandera = true;
+        float decimal = 0.5f;
+        double decimalPro = 0.5;
+        char caracter = 'A';
+        String saludo = "Hello World!";
+
+        System.out.println(saludo); //no es primitivo
+        System.out.println(numero);
+        System.out.println(otroNumero);
+        System.out.println(bandera);
+        System.out.println(decimal);
+        System.out.println(decimalPro);
+        System.out.println(caracter);
+
+        
+    }
+}
